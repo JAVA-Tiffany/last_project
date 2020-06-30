ca323c7254531adb48d3485f4309c1a2daec415e
bin <tjdqls45685210@gmail.com>
2020-06-30 14:05:33 +0900

    Tomcat 9.0 change

---
+++ src/main/java/com/project/controller/FaceBookController.java
@@ -0,0 +1,26 @@
+package com.project.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FaceBookController {
+	// 페이스북 oAuth 관련
+//    @Autowired
+//    private FacebookConnectionFactory connectionFactory;
+//    @Autowired
+//    private OAuth2Parameters oAuth2Parameters;
+// 
+//// join  뷰로 매핑
+//    @RequestMapping(value = "/join", method = { RequestMethod.GET, RequestMethod.POST })
+//    public String join(HttpServletResponse response, Model model) {
+//        
+//        OAuth2Operations oauthOperations = connectionFactory.getOAuthOperations();
+//        String facebook_url = oauthOperations.buildAuthorizeUrl(GrantType.AUTHORIZATION_CODE, oAuth2Parameters);
+//    
+//        model.addAttribute("facebook_url", facebook_url);
+//        System.out.println("/facebook" + facebook_url);
+// 
+//        return "member/join";
+//    }
+
+}
